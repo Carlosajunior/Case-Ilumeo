f5431686af7cd27522caf08347f479fa1db353c4
Carlosajunior <carlosajunior@hotmail.com>
2023-04-27 17:33:45 -0300

    eslint config

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
-import express from 'express';
-
-const app = express()
-const port = 3000
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
 app.get('/', (req, res) => {
-    res.send("Hello World")
-})
-
+    res.send("Hello World");
+});
 app.listen(port, () => {
-    console.log(`Aplicação rodando na porta ${port}`)
-})
+    console.log(`Aplicação rodando na porta ${port}`);
+});
